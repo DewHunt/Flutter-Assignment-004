381f37b2d97ed9b8784f29848f64a7140a63d342
dew <salman.giantssoft@gmail.com>
2024-05-21 04:33:17 +0600

    Flutter Assignment 004

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class PhotoGalleryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Photo Gallery App",
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
